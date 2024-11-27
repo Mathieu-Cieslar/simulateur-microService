406e8bc0b597f5a6cee1fda0fad8e8ead1d8273b
t9daNywe2h6WNfQryGtS <etienne.chaize@enfrasys.fr>
2024-11-27 16:42:09 +0100

    add lombock dependency

--- MicroService-Simulateur/src/main/java/fr/sdis/client/CasernClient.java
+++ MicroService-Simulateur/src/main/java/fr/sdis/client/CasernClient.java
@@ -5,7 +5,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.QueryParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import java.util.List;
--- MicroService-Simulateur/src/main/java/fr/sdis/service/FireService.java
+++ MicroService-Simulateur/src/main/java/fr/sdis/service/FireService.java
@@ -1,7 +1,13 @@
 package fr.sdis.service;
 
+import fr.sdis.dto.CaserneDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class FireService {
+
+    public void startFire() {
+
+    }
+
 }
