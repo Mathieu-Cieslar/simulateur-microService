8d284533af2fb91a1d751b0271af8a2168903aa3
t9daNywe2h6WNfQryGtS <etienne.chaize@enfrasys.fr>
2024-11-27 16:01:18 +0100

    add dependency for REST

--- MicroService-Simulateur/src/main/java/fr/sdis/client/SensorClient.java
+++ MicroService-Simulateur/src/main/java/fr/sdis/client/SensorClient.java
@@ -1,7 +1,14 @@
 package fr.sdis.client;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+@Path("/sensor")
 @ApplicationScoped
-public class SensorClient {
+@RegisterRestClient(configKey = "sim-api")
+public interface SensorClient {
+    @GET
+    String getSensorData();
 }
