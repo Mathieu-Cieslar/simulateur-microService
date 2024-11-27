35d0fca94136ff75d11d0723c8c772867adfc89a
t9daNywe2h6WNfQryGtS <etienne.chaize@enfrasys.fr>
2024-11-27 16:31:05 +0100

    working

---
+++ MicroService-Simulateur/src/main/java/fr/sdis/client/CasernClient.java
@@ -0,0 +1,24 @@
+package fr.sdis.client;
+
+import fr.sdis.dto.CaserneDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.util.List;
+
+@Path("/casern")
+@ApplicationScoped
+@RegisterRestClient(configKey = "sim-api")
+public interface CasernClient {
+    @GET
+    List<CaserneDTO> getCasernData();
+
+    @GET
+    @Path("/{idCasern}")
+    CaserneDTO getCasernById(@PathParam("idCasern") String idCasern);
+
+}
