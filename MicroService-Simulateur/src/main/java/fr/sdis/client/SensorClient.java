1026ce237e75a60e800eaab651c245e57ffbd35a
t9daNywe2h6WNfQryGtS <etienne.chaize@enfrasys.fr>
2024-11-27 15:07:12 +0100

    init

---
+++ MicroService-Simulateur/src/main/java/fr/sdis/client/SensorClient.java
@@ -0,0 +1,7 @@
+package fr.sdis.client;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class SensorClient {
+}
---
+++ MicroService-Simulateur/src/main/java/fr/sdis/service/FireService.java
@@ -0,0 +1,7 @@
+package fr.sdis.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class FireService {
+}
